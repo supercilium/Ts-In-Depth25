7768a2a523e7e3208fc9d6cc9d6558877c9e5936
Marusel <marusel@EvilMac.local>
2021-05-01 14:01:31 +0300

    [Task 2] add task 2

--- src/classes/library.ts
+++ src/classes/library.ts
@@ -1,5 +1,16 @@
+import { onChangeField } from "../decorators";
+
 export class Library {
-    id: number;
+    @onChangeField(Library.onChange) id: number;
     name: string;
     address: string;
+
+    @onChangeField(Library.onChange) email: string;
+
+    // static onChangeEmail(self: Library, newValue: string): void {
+    //     console.log(`${self.name} changed email from '${self.email}' to '${newValue}'`);
+    // }
+    static onChange(self: Library, newValue: string, propName: keyof Library): void {
+        console.log(`${self.name} changed ${propName} from '${self[propName]}' to '${newValue}'`);
+    }
 }
